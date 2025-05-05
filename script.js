587c2b032bb7a0e9331517e88b6195ce1281e67f
panwarvipul00 <panwarvipul1234@gmail.com>
2025-05-05 13:52:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-//your JS code here. If required.
 const status = document.getElementById("status");
 const enterBtn = document.getElementById("enterBtn");
 
 enterBtn.addEventListener("click", function () {
-	const h1 = document.createElement("h1");
-	h1.id = "status";
-	h1.textContent = "Entered Metaverse";
-	status.replaceWith(h1);
+  const h1 = document.createElement("h1");
+  h1.id = "status"; // Retain the same ID for testing
+  h1.textContent = "Entered Metaverse";
+  status.replaceWith(h1);
 });
+
 
