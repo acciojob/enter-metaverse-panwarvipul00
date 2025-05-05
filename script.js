7da670c44949b814f37e7cbb795326a53dcac93a
panwarvipul00 <panwarvipul1234@gmail.com>
2025-05-05 13:25:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const parag = document.getElementById("para");
+const btn = document.getElementById("butt");
+
+btn.addEventListener("click", function () {
+	const h1 = document.createElement("h1");
+	h1.id = "para";
+	h1.textContent = "Entered Metaverse";
+	parag.replaceWith(h1);
+});
+
